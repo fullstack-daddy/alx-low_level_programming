18ab6d6b66d6336c096dcb0b84daad2fb6dbfcab
fullstack-daddy <aboyejiemmanuel07@gmail.com>
2023-02-16 16:34:24 -0800

    finish work

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main - A program that print a line using the printf function
  * Return: 0 (success)
  */
-int main (void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of an art,\n");
+	printf("with proper grammar,but the outcome is a piece of an art,\n");
 return (0);
 }
