9c2d8aa86a242bb75230f1822c09b4e5e1e045c2
fullstack-daddy <aboyejiemmanuel07@gmail.com>
2023-02-27 05:51:48 -0800

    finish work

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,20 +7,21 @@
 */
 void print_rev(char *s)
 {
-		int longi = 0;
-			int o;
-
-				while (*s != '\0')
+	int longi = 0;
+	int o;
+	while (*s != '\0')
 						{
-									longi++;
-											s++;
-												}
-					s--;
-						for (o = longi; o > 0; o--)
-								{
-											_putchar(*s);
-													s--;
-														}
+							{
+										longi++;
+												s++;
+													}
+								s--;
+									for (o = longi; o > 0; o--)
+											{
+														_putchar(*s);
+																s--;
+																	}
 
-							_putchar('\n');
-}
+										_putchar('\n');
+						}
+
